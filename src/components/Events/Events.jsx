7d94f5c5789d390bf0facf117b7bdd53787bb2cd
marko <marko@marko.com>
2023-11-06 18:43:28 +0100

    adding layout-header cmp

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -2,14 +2,18 @@ import { Link, Outlet } from "react-router-dom";
 import EventsIntroSection from "./EventsIntroSection";
 import NewEventsSection from "./NewEventsSection";
 import FindEventSection from "./FindEventSection";
+import Header from "../Layout/Header";
 
 function Events() {
   return (
     <>
       <Outlet />
-      <Link to="events/new" className="button">
-        New Event
-      </Link>
+      <Header>
+        <Link to="events/new" className="button">
+          New Event
+        </Link>
+      </Header>
+
       <main>
         <EventsIntroSection />
         <NewEventsSection />
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line react/prop-types
+function Header({ children }) {
+  return (
+    <>
+      <div id="main-header-loading"></div>
+      <header id="main-header">
+        <div id="header-title">
+          <h1>React Events</h1>
+        </div>
+        <nav>{children}</nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
