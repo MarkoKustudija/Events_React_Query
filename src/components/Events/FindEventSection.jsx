7d9c94bc89a4e153ed4b8c02e42bf2ee60c50b6f
marko <marko@marko.com>
2023-11-06 21:12:52 +0100

    adding find-event-section

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,7 +1,28 @@
+import { useRef } from "react";
 
 function FindEventSection() {
+
+  const searchElement = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+
   return (
-    <div>FindEventSection</div>
+    <section className="content-section" id="all-events-section">
+      <header>
+        <h2>Find your next event!</h2>
+        <form onSubmit={handleSubmit} id ="search-form">
+          <input 
+          type="search"
+          placeholder="Search events"
+          ref={searchElement}/>
+          <button> Searh </button>
+        </form>
+        <p>Please enter a search term and to find events.</p>
+      </header>
+    </section>
   )
 }
 
